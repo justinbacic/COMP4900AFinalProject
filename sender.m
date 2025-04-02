9d144c5f615827dcb0ec7c8b5d2f3e7ec2a5d0b9
justinbacic <justinbacic@gmail.com>
2025-04-02 15:03:13 -0400

    added sender

---
+++ sender.m
@@ -0,0 +1,15 @@
+classdef sender < handle
+    % parent class for Alice and Bob
+    properties
+        %rectilinear basis and diagonal basis angles
+        basis = [['-','|'] ; ['/','\']]
+        %data bits
+        data = [];
+    end
+    methods
+        %calculate and return sifted key
+        function [key] = siftedKey(obj, Alice_bases, Bob_bases)
+            key = obj.data(find(Alice_bases==Bob_bases));
+        end
+    end
+end
