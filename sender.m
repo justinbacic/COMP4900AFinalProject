7b7e562a0dbf9aef8ce669249b83ffdf3e33ec82
justinbacic <justinbacic@gmail.com>
2025-04-02 15:16:20 -0400

    Made sender blank class template

--- sender.m
+++ sender.m
@@ -1,15 +1,6 @@
 classdef sender < communicator
-    % parent class for Alice and Bob
     properties
-        %rectilinear basis and diagonal basis angles
-        basis = [['-','|'] ; ['/','\']]
-        %data bits
-        data = [];
     end
     methods
-        %calculate and return sifted key
-        function [key] = siftedKey(obj, Alice_bases, Bob_bases)
-            key = obj.data(find(Alice_bases==Bob_bases));
-        end
     end
 end
