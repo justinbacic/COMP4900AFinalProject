850b3c94878359e1d65b13b217d9491b4e60e1d5
justinbacic <justinbacic@gmail.com>
2025-04-02 15:58:42 -0400

    Added communicator functions and properties

--- communicator.m
+++ communicator.m
@@ -1,9 +1,20 @@
 classdef communicator < handle
-
     properties
-        
+        permutations = {}
+        clifford_gates = {}
+        block_size = 0
+        sign_size = 0
     end
     methods
-
+        %Seeds the communicators random number generator
+        function setSeed(seed)
+            rng(seed)
+        end
+        function setBlockSize(obj, n)
+            obj.block_size = n;
+        end
+        function setSignSize(obj, l)
+            obj.sign_size = l;
+        end
     end
 end
